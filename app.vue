256a30ad9be168520faf3d1877986eb29b8737df
yodit93 <yoditabebe9391@gmail.com>
2024-11-05 15:36:32 +0300

    Create nuxt app

---
+++ app.vue
@@ -0,0 +1,12 @@
+<template>
+  <NuxtLayout>
+    <header>
+      <NuxtLink to="/">Home</NuxtLink>
+      <NuxtLink to="/events">Events</NuxtLink>
+    </header>
+    <NuxtPage />
+    <footer>
+      <p>&copy; 2024</p>
+    </footer>
+  </NuxtLayout>
+</template>
