f1c7da8ca35bfd29d09964243208dcb1072defa6
yodit93 <yoditabebe9391@gmail.com>
2024-11-05 15:36:45 +0300

    Add pages

---
+++ pages/events.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+</script>
+
+<template>
+   <div>
+    <h1>Events page</h1>
+   </div> 
+</template>
+
+<style lang="css" scoped>
+    
+</style>
+
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+    <div>
+        <h1>Home page</h1>
+    </div>
+</template>
+
+<style lang="postcss" scoped>
+    
+</style>
