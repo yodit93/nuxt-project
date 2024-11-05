e7fd0cba774d92995bc2ae62e5d283042dd8ef3e
yodit93 <yoditabebe9391@gmail.com>
2024-11-05 16:52:31 +0300

    Assets vs Public folder

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import {resolve} from 'path'
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  alias: {
+    'assets': resolve(__dirname, 'assets'),
+  },
 })
