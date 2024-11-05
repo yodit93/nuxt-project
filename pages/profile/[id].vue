a292a89a85a922d69b0181e45ac40b16fca7c449
yodit93 <yoditabebe9391@gmail.com>
2024-11-05 15:44:17 +0300

    Use dynamic pages

---
+++ pages/profile/[id].vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+    const route = useRoute();
+    console.log(route.params.id);
+</script>
+
+<template>
+    <div>
+        <h1>{{$route.params.id}}</h1>
+    </div>
+</template>
+
+<style lang="postcss" scoped>
+    
+</style>
