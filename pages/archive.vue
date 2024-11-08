82015f7c62e6019fbead66341d9ff6c47df59cad
yodit93 <yoditabebe9391@gmail.com>
2024-11-08 17:42:54 +0300

    Implement functionality for the archive page

--- pages/archive.vue
+++ pages/archive.vue
@@ -1,11 +1,36 @@
 <script setup>
+    const { archivedEmails, selectedEmails, selectAllEmails, isEmailSelected } = useEmailStore();
 
+    const areAllSelected = computed( () => selectedEmails.value.length === archivedEmails.value.length && archivedEmails.value.length > 0);
+
+    const deleteEmail = () => {
+        selectedEmails.value.forEach(email => {
+            archivedEmails.value = archivedEmails.value.filter(archivedEmail => archivedEmail !== email);
+        });
+        selectedEmails.value = [];
+    };
 </script>
 
 <template>
-    <div>archive page</div>
+    <section class="email-page">
+        <h2>Archive</h2>
+        <header class="email-page__header">
+           <div class="email-page__header--selection">
+            <input type="checkbox" @click="e => selectAllEmails(e, archivedEmails)" :checked="areAllSelected">
+            <p>Email Selected (0)</p>
+           </div>
+           <div class="email-page__header--actions" v-if="isEmailSelected">
+            <button class="email-page__mark-read" @click="deleteEmail">
+                <img src="/trash-01.png" alt="mail">
+                <span>Delete</span>
+            </button>
+           </div>
+        </header>
+        <ul>
+            <li v-for="email in archivedEmails" :key="email.title">
+                <input type="checkbox" :value="email" v-model="selectedEmails">
+                <p>{{ email.title }}</p>
+            </li>
+        </ul>
+    </section>
 </template>
-
-<style scoped>
-    
-</style>
