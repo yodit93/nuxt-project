eb18941f89b6b511a39b4fe298f7717eb6abaf7c
yodit93 <yoditabebe9391@gmail.com>
2024-11-09 17:51:40 +0300

    Addoverlay when the deatil page is rendered

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,25 @@
+<script setup>
+   const {isEmailOpened} = useEmailStore();
+</script>
 <template>
     <div class="container">
+        <div v-if="isEmailOpened" class="overlay"></div>
         <Sidebar />
-        <slot />
-          
+        <slot />     
     </div>
 </template>
  <style lang="scss" scoped>   
     .container {
         display: flex;
+    }
+
+    .overlay {
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100vw;
+        height: 100vh;
+        background: rgb(116 133 154 / 80%);
+        z-index: 10;
     }
  </style>
